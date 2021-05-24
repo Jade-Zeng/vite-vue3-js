3cae6d99abf92597c3cadcff016ea5e9f327b941
jade <jade.zeng@d1m.cn>
2021-05-24 18:38:01 +0800

    ref

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HelloWorld msg="Hello" :item="obj"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      obj: {
+        count: 1000
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <button @click="num++">{{num}}</button>
+  <button @click="Add">{{sta.cou}}</button>
+</template>
+
+<script>
+  import {ref, reactive, toRef, toRefs} from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    item: Object
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  setup (prop) {
+    const num = ref(0)
+
+    const sta = reactive({cou: 99})
+
+    const Add = () => {
+      sta.cou++
+    }
+
+    const n1 = toRefs(prop)
+    // console.log(n1.item.value.counts
+
+    return {
+      num,
+      sta,
+      Add,
+      n1
+    }
+  }
+}
+</script>
